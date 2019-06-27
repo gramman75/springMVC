d34a9b3f25c216f35e38124535ba4746adf39aab
gramman75 <kmk7504>
2019-06-28 00:34:51 +0900

    init

---
+++ springMVC/src/main/java/com/gramman75/HelloService.java
@@ -0,0 +1,10 @@
+package com.gramman75;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+    public String getName() {
+        return "Gramman75";
+    }
+}
